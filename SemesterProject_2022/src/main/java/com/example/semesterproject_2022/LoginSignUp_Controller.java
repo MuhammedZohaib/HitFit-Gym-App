f7090cac21de140b82b3dc8dab4f7a3b606aaddd
codefolk <alizuhaib828@gmail.com>
2022-12-03 00:36:23 +0500

    Working on SignUp form

--- SemesterProject_2022/src/main/java/com/example/semesterproject_2022/LoginSignUp_Controller.java
+++ SemesterProject_2022/src/main/java/com/example/semesterproject_2022/LoginSignUp_Controller.java
@@ -1,4 +1,20 @@
 package com.example.semesterproject_2022;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginSignUp_Controller {
+    public void switchToSignUp(ActionEvent e) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignUp.fxml"));
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
