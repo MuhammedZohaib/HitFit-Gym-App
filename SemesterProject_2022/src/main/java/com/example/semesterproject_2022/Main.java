d835bcf75e4f61bb9e094470df017895392a02da
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2022-12-13 22:35:45 +0500

    Changed FXML in main

--- SemesterProject_2022/src/main/java/com/example/semesterproject_2022/Main.java
+++ SemesterProject_2022/src/main/java/com/example/semesterproject_2022/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignUp_Prompt.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoadingScreen.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
