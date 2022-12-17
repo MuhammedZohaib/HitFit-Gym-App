990bcae4ae0edd027e4ce823199f9b0a46929ce7
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2022-12-18 04:58:14 +0500

    imports

--- SemesterProject_2022/src/main/java/com/example/semesterproject_2022/LogIn_Form_Controller.java
+++ SemesterProject_2022/src/main/java/com/example/semesterproject_2022/LogIn_Form_Controller.java
@@ -2,6 +2,9 @@ package com.example.semesterproject_2022;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -9,6 +12,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogIn_Form_Controller {
 
