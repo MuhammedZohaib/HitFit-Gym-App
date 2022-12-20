ec38f5f825d4dc72b8e2d2fda0902dbaaf071224
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2022-12-20 16:00:18 +0500

    Dashboard Stage Modified
    Added Border and Functionality to the border buttons i.e Our Close Button, Max Button, Mini Button

--- SemesterProject_2022/src/main/java/com/example/semesterproject_2022/Main.java
+++ SemesterProject_2022/src/main/java/com/example/semesterproject_2022/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignUp.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogIn_Form.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
--- SemesterProject_2022/src/main/java/com/example/semesterproject_2022/Main_Interface.java
+++ SemesterProject_2022/src/main/java/com/example/semesterproject_2022/Main_Interface.java
@@ -1,6 +1,43 @@
 package com.example.semesterproject_2022;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 public class Main_Interface {
+    LoadingScreen_Controller obj = new LoadingScreen_Controller();
+    Rectangle2D screenres = Screen.getPrimary().getVisualBounds();
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button minimize;
+
+    /*----Windows Border Icons Functionality------------*/
+
+    @FXML
+    void close(ActionEvent event) {
+        obj.stage = (Stage) exit.getScene().getWindow();
+        obj.stage.close();
+    }
+
+
+    @FXML
+    void maximizebtn(ActionEvent event) {
+
+        
+    }
+
+    @FXML
+    void minimzebtn(ActionEvent event) {
+        obj.stage=(Stage) minimize.getScene().getWindow();
+        obj.stage.setIconified(true);
+    }
 
 }
