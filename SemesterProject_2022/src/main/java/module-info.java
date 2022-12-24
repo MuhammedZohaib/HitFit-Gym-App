af756ee4fac2d3cb4908fc02c9decb5d2c5d4cda
@Ayaan08 <94982193+Scr1pt-K1ddie@users.noreply.github.com>
2022-12-24 16:04:35 +0500

    fixed errors

--- SemesterProject_2022/src/main/java/module-info.java
+++ SemesterProject_2022/src/main/java/module-info.java
@@ -2,10 +2,8 @@ module com.example.semesterproject_2022 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-//    requires org.apache.commons.codec;
+    requires org.apache.commons.codec;
     requires com.mailjet.api;
-    requires commons.codec;
-
 
     opens com.example.semesterproject_2022 to javafx.fxml;
     exports com.example.semesterproject_2022;
