a277e5120073b322453a16bb1aa1d636d4183bbd
codefolk <alizuhaib828@gmail.com>
2022-12-25 21:52:44 +0500

    Changed size of Dashboard

--- SemesterProject_2022/src/main/java/com/example/semesterproject_2022/GeneralFunctions.java
+++ SemesterProject_2022/src/main/java/com/example/semesterproject_2022/GeneralFunctions.java
@@ -3,11 +3,9 @@ package com.example.semesterproject_2022;
 import com.ResizeHelper.ResizeHelper;
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
