7024ac01878a4cd52096538cd02133657eb180a7
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2023-01-06 22:06:23 +0500

    employees

--- SemesterProject_2022/src/main/java/com/example/semesterProject_2022/EmployeesPanel_Controller.java
+++ SemesterProject_2022/src/main/java/com/example/semesterProject_2022/EmployeesPanel_Controller.java
@@ -2,15 +2,80 @@ package com.example.semesterProject_2022;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class EmployeesPanel_Controller {
-    @FXML
-    private Text texttbchanged;
 
     @FXML
-    void changetxt(ActionEvent event) {
-        texttbchanged.setText("This is Employees Panel");
+    private TableColumn<?, ?> Designation;
+
+    @FXML
+    private TableColumn<?, ?> FirstName;
+
+    @FXML
+    private TableColumn<?, ?> Id;
+
+    @FXML
+    private TableColumn<?, ?> LastName;
+
+    @FXML
+    private TableColumn<?, ?> Salary;
+
+    @FXML
+    private TableColumn<?, ?> action;
+
+    @FXML
+    private Button addbutton;
+
+    @FXML
+    private TableColumn<?, ?> email;
+
+    @FXML
+    private TableView<?> employeesView;
+
+    @FXML
+    private TextField keyword;
+
+    @FXML
+    private TableColumn<?, ?> nic;
+
+    @FXML
+    private Pagination pagination;
+
+    @FXML
+    private TableColumn<?, ?> phone;
+
+    @FXML
+    private Button refreshButton;
+
+    @FXML
+    private Button sortButton;
+
+    @FXML
+    private Button sortButton1;
+
+    @FXML
+    void addEmployee(ActionEvent event) {
 
     }
+
+    @FXML
+    void refreshbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sortbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sortbtn1(ActionEvent event) {
+
+    }
+
 }
