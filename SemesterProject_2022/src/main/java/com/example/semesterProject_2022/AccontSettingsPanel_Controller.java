4c4acd8a24ef84f47af983e69d443711ed94d883
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2023-01-11 02:29:56 +0500

    Account Settings Panel Created

--- SemesterProject_2022/src/main/java/com/example/semesterProject_2022/AccontSettingsPanel_Controller.java
+++ SemesterProject_2022/src/main/java/com/example/semesterProject_2022/AccontSettingsPanel_Controller.java
@@ -7,11 +7,37 @@ import javafx.scene.text.Text;
 public class AccontSettingsPanel_Controller {
 
     @FXML
-    private Text texttbchanged;
+    private Text emailField;
 
     @FXML
-    void changetxt(ActionEvent event) {
-        texttbchanged.setText("This is Account Settings ");
+    private Text emailField1;
+
+    @FXML
+    private Text emailField11;
+
+    @FXML
+    private Text nameField;
+
+    @FXML
+    private Text phoneField;
+
+    @FXML
+    void editEmailButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editNameButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editPhoneButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updatePasswordButton(ActionEvent event) {
 
     }
 
