1e949e0d49fa7d3f4cdb03a4f1cecff8b9e04c0e
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2023-01-13 07:10:31 +0500

    changes

---
+++ SemesterProject_2022/src/main/java/com/example/semesterProject_2022/QueriesReply_Controller.java
@@ -0,0 +1,51 @@
+package com.example.semesterProject_2022;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+import javax.security.auth.Destroyable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QueriesReply_Controller implements Initializable {
+
+    @FXML
+    private Text Heading;
+
+    @FXML
+    private TextArea ReplyBox;
+
+    @FXML
+    private Text description;
+
+    @FXML
+    private Button exitButton;
+    private String reply;
+    @FXML
+    void ReplyQueryBtn(ActionEvent event) {
+
+        reply = ReplyBox.getText();
+
+
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+        new GeneralFunctions().close(exitButton);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Heading.setText(" ");
+        description.setText(" ");
+
+    }
+
+}
